5d0d533a9138af18dadb522f5e07f1b4f6b43a40
EtherSystem <dermeisternew@gmail.com>
2026-01-18 03:38:36 +0100

    - New acclimatization mechanics - Altitude effects now apply to certain afflictions - Altitude effects now apply to the cooldown time before stamina regeneration - Revised player ingame presence detection logic - Oxygen levels in indoor scenes are now relative to the level of the last visited outdoor scene - New customization sliders

--- OxygenLevels/Core.cs
+++ OxygenLevels/Core.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using LocalizationUtilities;
 using ModComponent;
 
-[assembly: MelonInfo(typeof(OxygenLevels.Core), "OxygenLevels", "1.1.6", "EtherSystem", null)]
+[assembly: MelonInfo(typeof(OxygenLevels.Core), "OxygenLevels", "1.2.0", "EtherSystem", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
 
 namespace OxygenLevels
@@ -15,7 +15,8 @@ namespace OxygenLevels
     {
         public static bool isInterHUD = false;
         private float timeSpentAtCritAltitude = 0f;
-        private float SUFFOCATION_THRESHOLD_HOURS = 1f;
+        private readonly float SUFFOCATION_THRESHOLD_HOURS = 1f;
+        private float lastOutdoorAltitude = 0f;
         public static string? LoadEmbeddedJSON(string Localization)
         {
             string? result = null;
@@ -35,11 +36,19 @@ namespace OxygenLevels
             Settings.OnLoad();
         }
         private float updateTimer = 0f;
-        private float baseStaminaRegenRate = 500f;
-        private float baseStaminaSprintUsageRate = 5f;
-        private float baseMaxFatigueSprintUsageRate = 150f;
-        private float baseMinFatigueSprintUsageRate = 1f;
+        private readonly float baseDelayRecoverStam = 2f;
+        private readonly float baseStaminaRegenRate = 500f;
+        private readonly float baseStaminaSprintUsageRate = 5f;
+        private readonly float baseMaxFatigueSprintUsageRate = 150f;
+        private readonly float baseMinFatigueSprintUsageRate = 1f;
         private float defaultFireIgnitionTime = -1f;
+        private readonly float baseDysenteryMaxRecoveryTime = 24f;
+        private readonly float baseDysenteryMinRecoveryTime = 18f;
+        private readonly float baseFoodPoisoningMaxRecoveryTime = 24f;
+        private readonly float baseFoodPoisoningMinRecoveryTime = 12f;
+        private float acclimatationTimerHours = 0f;
+        private bool isAcclimatized = false;
+        private bool wasAcclimatizedLastFrame = false;
 
         public enum AltitudeState { Normal, Weakened, HeavyWeakened, TooWeak }
         public static AltitudeState currentState = AltitudeState.Normal;
@@ -51,15 +60,48 @@ namespace OxygenLevels
             float realTimeElapsed = updateTimer;
             updateTimer = 0f;
 
-            if (GameManager.GetPlayerTransform() == null) return;
+            if (GameManager.m_Instance == null || GameManager.m_IsPaused) return;
+            string scene = GameManager.m_ActiveScene;
+            if (scene == "MainMenu" || scene == "Boot" || scene == "Empty") return;
 
             if (defaultFireIgnitionTime < 0f)
             {
                 defaultFireIgnitionTime = GameManager.m_FireManager.m_StartFireTimeSeconds;
             }
-            float yValue = GameManager.GetPlayerTransform().position.y;
-            ApplyAltitudeEffects(yValue);
+            float rawY = GameManager.GetPlayerTransform().position.y;
+            bool isIndoors = GameManager.GetWeatherComponent().IsIndoorScene();
+            if (!isIndoors)
+            {
+                lastOutdoorAltitude = rawY;
+            }
+            float yValue = isIndoors ? lastOutdoorAltitude : rawY;
+
             float gameHoursPassed = GameManager.GetTimeOfDayComponent().GetTODHours(realTimeElapsed);
+            float HOURS_TO_ACCLIMATIZE = (Settings.options.AcclimatizationTimer);
+
+            if (currentState >= AltitudeState.HeavyWeakened)
+            {
+                acclimatationTimerHours += gameHoursPassed;
+            }
+            else if (currentState <= AltitudeState.Weakened)
+            {
+                acclimatationTimerHours -= gameHoursPassed * 2f;
+            }
+            acclimatationTimerHours = Mathf.Clamp(acclimatationTimerHours, 0f, HOURS_TO_ACCLIMATIZE);
+            isAcclimatized = (acclimatationTimerHours >= HOURS_TO_ACCLIMATIZE);
+            //MelonLogger.Msg("base : " + acclimatationTimerHours);
+
+            if (isAcclimatized && !wasAcclimatizedLastFrame)
+            {
+                HUDMessage.AddMessage(Localization.Get("GAMEPLAY_isAcclimatized"), 5, false);
+            }
+            if (!isAcclimatized && wasAcclimatizedLastFrame)
+            {
+                HUDMessage.AddMessage(Localization.Get("GAMEPLAY_isntAcclimatized"), 5, false);
+            }
+            wasAcclimatizedLastFrame = isAcclimatized;
+
+            ApplyAltitudeEffects(yValue);
 
             if (currentState == AltitudeState.TooWeak)
             {
@@ -128,6 +170,9 @@ namespace OxygenLevels
                 float maxFatigueBurnMultiplier = 1f;
                 float minFatigueBurnMultiplier = 1f;
                 float fireIgnitionMultiplier = 1f;
+                float secondsBeforeRecovStamMultiplier = 1f;
+                float dysenteryRecoveryTimeMultiplier = 1f;
+                float foodPoisoningRecoveryTimeMultiplier = 1f;
 
                 switch (currentState)
                 {
@@ -137,29 +182,70 @@ namespace OxygenLevels
                         maxFatigueBurnMultiplier = Settings.options.LowMaxFatigueBurnMultiplier;
                         minFatigueBurnMultiplier = Settings.options.LowMinFatigueBurnMultiplier;
                         fireIgnitionMultiplier = Settings.options.LowFireIgnitionMultiplier;
+                        secondsBeforeRecovStamMultiplier = Settings.options.LowSecondsBeforeRecovStamMultiplier;
+                        dysenteryRecoveryTimeMultiplier = Settings.options.LowDysenteryRecoveryTimeMultiplier;
+                        foodPoisoningRecoveryTimeMultiplier = Settings.options.LowFoodPoisoningRecoveryTimeMultiplier;
                         HUDMessage.AddMessage(Localization.Get("GAMEPLAY_LowWarning"), 5, false);
                         break;
                     case AltitudeState.HeavyWeakened:
-                        staminaMultiplier = Settings.options.CritStaminaMultiplier;
-                        staminaConsumptionMultiplier = Settings.options.CritStaminaConsumptionMultiplier;
-                        maxFatigueBurnMultiplier = Settings.options.CritMaxFatigueBurnMultiplier;
-                        minFatigueBurnMultiplier = Settings.options.CritMinFatigueBurnMultiplier;
-                        fireIgnitionMultiplier = Settings.options.CritFireIgnitionMultiplier;
-                        HUDMessage.AddMessage(Localization.Get("GAMEPLAY_CriticalWarning"), 5, false);
+                        if (isAcclimatized)
+                        {
+                            staminaMultiplier = (Settings.options.CritStaminaMultiplier * 1.5f);
+                            staminaConsumptionMultiplier = (Settings.options.CritStaminaConsumptionMultiplier * 0.5f);
+                            maxFatigueBurnMultiplier = (Settings.options.CritMaxFatigueBurnMultiplier * 0.5f);
+                            minFatigueBurnMultiplier = (Settings.options.CritMinFatigueBurnMultiplier * 0.5f);
+                            fireIgnitionMultiplier = (Settings.options.CritFireIgnitionMultiplier * 0.5f);
+                            secondsBeforeRecovStamMultiplier = (Settings.options.CritSecondsBeforeRecovStamMultiplier * 0.5f);
+                            dysenteryRecoveryTimeMultiplier = (Settings.options.CritDysenteryRecoveryTimeMultiplier * 0.5f);
+                            foodPoisoningRecoveryTimeMultiplier = (Settings.options.CritFoodPoisoningRecoveryTimeMultiplier * 0.5f);
+                        }
+                        else
+                        {
+                            staminaMultiplier = Settings.options.CritStaminaMultiplier;
+                            staminaConsumptionMultiplier = Settings.options.CritStaminaConsumptionMultiplier;
+                            maxFatigueBurnMultiplier = Settings.options.CritMaxFatigueBurnMultiplier;
+                            minFatigueBurnMultiplier = Settings.options.CritMinFatigueBurnMultiplier;
+                            fireIgnitionMultiplier = Settings.options.CritFireIgnitionMultiplier;
+                            secondsBeforeRecovStamMultiplier = Settings.options.CritSecondsBeforeRecovStamMultiplier;
+                            dysenteryRecoveryTimeMultiplier = Settings.options.CritDysenteryRecoveryTimeMultiplier;
+                            foodPoisoningRecoveryTimeMultiplier = Settings.options.CritFoodPoisoningRecoveryTimeMultiplier;
+                            HUDMessage.AddMessage(Localization.Get("GAMEPLAY_CriticalWarning"), 5, false);
+                        }
                         break;
                     case AltitudeState.TooWeak:
-                        staminaMultiplier = (Settings.options.InsuStaminaMultiplier / 10);
-                        staminaConsumptionMultiplier = Settings.options.InsuStaminaConsumptionMultiplier;
-                        maxFatigueBurnMultiplier = Settings.options.InsuMaxFatigueBurnMultiplier;
-                        minFatigueBurnMultiplier = Settings.options.InsuMinFatigueBurnMultiplier;
-                        fireIgnitionMultiplier = Settings.options.InsuFireIgnitionMultiplier;
-                        HUDMessage.AddMessage(Localization.Get("GAMEPLAY_InsufficientWarning"), 5, false);
+                        if (isAcclimatized)
+                        {
+                            staminaMultiplier = ((Settings.options.InsuStaminaMultiplier / 10) * 1.5f);
+                            staminaConsumptionMultiplier = (Settings.options.InsuStaminaConsumptionMultiplier * 0.5f);
+                            maxFatigueBurnMultiplier = (Settings.options.InsuMaxFatigueBurnMultiplier * 0.5f);
+                            minFatigueBurnMultiplier = (Settings.options.InsuMinFatigueBurnMultiplier * 0.5f);
+                            fireIgnitionMultiplier = (Settings.options.InsuFireIgnitionMultiplier * 0.5f);
+                            secondsBeforeRecovStamMultiplier = (Settings.options.InsuSecondsBeforeRecovStamMultiplier * 0.5f);
+                            dysenteryRecoveryTimeMultiplier = (Settings.options.InsuDysenteryRecoveryTimeMultiplier * 0.5f);
+                            foodPoisoningRecoveryTimeMultiplier = (Settings.options.InsuFoodPoisoningRecoveryTimeMultiplier * 0.5f);
+                        }
+                        else
+                        {
+                            staminaMultiplier = (Settings.options.InsuStaminaMultiplier / 10);
+                            staminaConsumptionMultiplier = Settings.options.InsuStaminaConsumptionMultiplier;
+                            maxFatigueBurnMultiplier = Settings.options.InsuMaxFatigueBurnMultiplier;
+                            minFatigueBurnMultiplier = Settings.options.InsuMinFatigueBurnMultiplier;
+                            fireIgnitionMultiplier = Settings.options.InsuFireIgnitionMultiplier;
+                            secondsBeforeRecovStamMultiplier = Settings.options.InsuSecondsBeforeRecovStamMultiplier;
+                            dysenteryRecoveryTimeMultiplier = Settings.options.InsuDysenteryRecoveryTimeMultiplier;
+                            foodPoisoningRecoveryTimeMultiplier = Settings.options.InsuFoodPoisoningRecoveryTimeMultiplier;
+                            HUDMessage.AddMessage(Localization.Get("GAMEPLAY_InsufficientWarning"), 5, false);
+                        }
                         break;
                     case AltitudeState.Normal:
                         staminaMultiplier = 1f;
                         staminaConsumptionMultiplier = 1f;
                         maxFatigueBurnMultiplier = 1f;
                         minFatigueBurnMultiplier = 1f;
+                        fireIgnitionMultiplier = 1f;
+                        secondsBeforeRecovStamMultiplier = 1f;
+                        dysenteryRecoveryTimeMultiplier = 1f;
+                        foodPoisoningRecoveryTimeMultiplier = 1f;
                         HUDMessage.AddMessage(Localization.Get("GAMEPLAY_NormalizedWarning"), 5, false);
                         break;
                 }
@@ -168,6 +254,13 @@ namespace OxygenLevels
                 GameManager.GetFatigueComponent().m_FatigueIncreasePerHourSprintingMax = baseMaxFatigueSprintUsageRate * maxFatigueBurnMultiplier;
                 GameManager.GetFatigueComponent().m_FatigueIncreasePerHourSprintingMin = baseMinFatigueSprintUsageRate * minFatigueBurnMultiplier;
                 GameManager.m_FireManager.m_StartFireTimeSeconds = defaultFireIgnitionTime * fireIgnitionMultiplier;
+                GameManager.GetPlayerMovementComponent().m_SecondsNotSprintingBeforeRecovery = baseDelayRecoverStam * secondsBeforeRecovStamMultiplier;
+                GameManager.GetDysenteryComponent().m_DurationHoursMax = baseDysenteryMaxRecoveryTime * dysenteryRecoveryTimeMultiplier;
+                GameManager.GetDysenteryComponent().m_DurationHoursMin = baseDysenteryMinRecoveryTime * dysenteryRecoveryTimeMultiplier;
+                GameManager.GetFoodPoisoningComponent().m_DurationHoursMax = baseFoodPoisoningMaxRecoveryTime * foodPoisoningRecoveryTimeMultiplier;
+                GameManager.GetFoodPoisoningComponent().m_DurationHoursMin = baseFoodPoisoningMinRecoveryTime * foodPoisoningRecoveryTimeMultiplier;
+                
+                //MelonLogger.Msg("default : " + GameManager.GetConditionComponent().m_HPIncreasePerDayWhileHealthy);
             }
         }
     }
--- OxygenLevels/OxModSettings.cs
+++ OxygenLevels/OxModSettings.cs
@@ -16,6 +16,14 @@ namespace OxygenLevels
         [Description("A game restart will be required.")]
         public bool interHUD = false;
 
+        //Acclimatization
+        [Section("Acclimatization")]
+
+        [Name("Time needed to acclimatize")]
+        [Description("Base = 24 - ingame hours")]
+        [Slider(1, 72)]
+        public float AcclimatizationTimer = 24f;
+
         //Low o₂
         [Section("Low o₂")]
 
@@ -26,29 +34,43 @@ namespace OxygenLevels
 
         [Name("Stamina recovery speed")]
         [Description("Base = 0.6")]
-        [Slider(0, 1)]
+        [Slider(0.1f, 1)]
         public float LowStaminaMultiplier = 0.6f;
 
         [Name("Stamina consumption speed")]
         [Description("Base = 1.5")]
-        [Slider(1, 10)]
+        [Slider(1.5f, 10)]
         public float LowStaminaConsumptionMultiplier = 1.5f;
 
         [Name("Minimum fatigue consumption speed")]
         [Description("Base = 4")]
-        [Slider(1, 100)]
+        [Slider(2, 100)]
         public float LowMinFatigueBurnMultiplier = 4f;
 
         [Name("Maximum fatigue consumption speed")]
         [Description("Base = 4")]
-        [Slider(1, 100)]
+        [Slider(2, 100)]
         public float LowMaxFatigueBurnMultiplier = 4f;
 
         [Name("Fire ignition multiplier")]
         [Description("Base = 2")]
-        [Slider(1, 10)]
+        [Slider(2, 10)]
         public float LowFireIgnitionMultiplier = 2f;
 
+        [Name("Seconds before recovering stamina multiplier")]
+        [Description("Base = 2")]
+        [Slider(2, 20)]
+        public float LowSecondsBeforeRecovStamMultiplier = 2f;
+
+        [Name("Dysentery recovery time multiplier")]
+        [Description("Base = 1.5")]
+        [Slider(1.5f, 20)]
+        public float LowDysenteryRecoveryTimeMultiplier = 1.5f;
+
+        [Name("Food poisoning recovery time multiplier")]
+        [Description("Base = 1.5")]
+        [Slider(1.5f, 20)]
+        public float LowFoodPoisoningRecoveryTimeMultiplier = 1.5f;
 
         //Critical o₂
         [Section("Critical o₂")]
@@ -60,29 +82,43 @@ namespace OxygenLevels
 
         [Name("Stamina recovery speed")]
         [Description("Base = 0.3")]
-        [Slider(0, 1)]
+        [Slider(0.1f, 1)]
         public float CritStaminaMultiplier = 0.3f;
 
         [Name("Stamina consumption speed")]
         [Description("Base = 2.5")]
-        [Slider(1, 10)]
+        [Slider(2, 10)]
         public float CritStaminaConsumptionMultiplier = 2.5f;
 
         [Name("Minimum fatigue consumption speed")]
         [Description("Base = 10")]
-        [Slider(1, 100)]
+        [Slider(2, 100)]
         public float CritMinFatigueBurnMultiplier = 10f;
 
         [Name("Maximum fatigue consumption speed")]
         [Description("Base = 10")]
-        [Slider(1, 100)]
+        [Slider(2, 100)]
         public float CritMaxFatigueBurnMultiplier = 10f;
 
         [Name("Fire ignition multiplier")]
         [Description("Base = 3")]
-        [Slider(1, 10)]
+        [Slider(2, 10)]
         public float CritFireIgnitionMultiplier = 3f;
 
+        [Name("Seconds before recovering stamina multiplier")]
+        [Description("Base = 3")]
+        [Slider(2, 20)]
+        public float CritSecondsBeforeRecovStamMultiplier = 3f;
+
+        [Name("Dysentery recovery time multiplier")]
+        [Description("Base = 2")]
+        [Slider(2, 20)]
+        public float CritDysenteryRecoveryTimeMultiplier = 2f;
+
+        [Name("Food poisoning recovery time multiplier")]
+        [Description("Base = 2")]
+        [Slider(2, 20)]
+        public float CritFoodPoisoningRecoveryTimeMultiplier = 2f;
 
         //Insufficient o₂
         [Section("Insufficient o₂")]
@@ -99,18 +135,38 @@ namespace OxygenLevels
 
         [Name("Stamina consumption speed")]
         [Description("Base = 3")]
-        [Slider(1, 10)]
+        [Slider(2, 10)]
         public float InsuStaminaConsumptionMultiplier = 3f;
 
         [Name("Minimum fatigue consumption speed")]
         [Description("Base = 20")]
-        [Slider(1, 100)]
+        [Slider(2, 100)]
         public float InsuMinFatigueBurnMultiplier = 20f;
 
         [Name("Maximum fatigue consumption speed")]
         [Description("Base = 20")]
-        [Slider(1, 100)]
+        [Slider(2, 100)]
         public float InsuMaxFatigueBurnMultiplier = 20f;
+
+        [Name("Fire ignition multiplier")]
+        [Description("Base = 5")]
+        [Slider(2, 10)]
+        public float InsuFireIgnitionMultiplier = 5f;
+
+        [Name("Seconds before recovering stamina multiplier")]
+        [Description("Base = 5")]
+        [Slider(2, 20)]
+        public float InsuSecondsBeforeRecovStamMultiplier = 5f;
+
+        [Name("Dysentery recovery time multiplier")]
+        [Description("Base = 3")]
+        [Slider(2, 20)]
+        public float InsuDysenteryRecoveryTimeMultiplier = 3f;
+
+        [Name("Food poisoning recovery time multiplier")]
+        [Description("Base = 3")]
+        [Slider(2, 20)]
+        public float InsuFoodPoisoningRecoveryTimeMultiplier = 3f;
 
         [Name("Stamina consumption when walking")]
         [Description("Base = 0.1")]
@@ -121,11 +177,6 @@ namespace OxygenLevels
         [Description("Base = 0.5 / 10")]
         [Slider(0.5f, 10f, 95)]
         public float ConditionLostZeroStamina = 0.5f;
-
-        [Name("Fire ignition multiplier")]
-        [Description("Base = 5")]
-        [Slider(1, 10)]
-        public float InsuFireIgnitionMultiplier = 5f;
 
         [Name("AMS appearance time")]
         [Description("Base = 2 - ingame hours")]
